b9397337e48778f0804a7c95027aba0d56587b7f
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-05 13:02:23 +0200

    stable version

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,64 +4,58 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-
-// components
 import LandingPage from './Components/LandingPage';
 import AboutPage from './Components/Pages/About/AboutPage';
 import Navbar from './Components/Layout/Navbar';
 import MagazineListPage from './Components/Pages/Magazine/MagazineListPage';
 import BlogListPage from './Components/Pages/Blog/BlogListPage';
-import Juniors from './Components/Pages/Juniors/JuniorsPage';
-import WIE from './Components/Pages/WIE/WIEPage';
+
+import Juniors from './Components/Pages/Juniors/JuniorsPage'
+import WIE from './Components/Pages/WIE/WIEPage'
+
 import BlogPage from './Components/Pages/Blog/BlogPage';
 
 function App() {
-	const NavbarProps = {
-		NavItems: [
-			{ id: 1, name: 'Home', href: '/' },
-			{ id: 2, name: 'About', href: '/about' },
-			{ id: 3, name: 'Magazine', href: '/magazine' },
-			{ id: 4, name: 'Blog', href: '/blog' },
-			{ id: 5, name: 'WIE', href: '/WIE' },
-			{ id: 6, name: 'Juniors', href: '/juniors' },
-		],
-		PageLink: '/',
-		isSearchable: false,
-		socialLinks: true,
-		Static: true,
+  const NavbarProps = {
+    NavItems: [
+      { id: 1, name: "Home", href: "/" },
+      { id: 2, name: "About", href: "/about" },
+      { id: 3, name: "Magazine", href: "/magazine" },
+      { id: 4, name: "Blog", href: "/blog" },
+      { id: 5, name: "WIE", href: "/WIE" },
+      { id: 6, name: "Juniors", href: "/juniors" },
 
-		socialAccounts: [
-			{
-				website: 'fab fa-facebook',
-				href: 'https://www.facebook.com/IEEEZC/',
-			},
-			{
-				website: 'fab fa-twitter',
-				href: 'https://twitter.com/IEEEZC',
-			},
-			{
-				website: 'fab fa-youtube',
-				href: 'https://www.youtube.com/user/IEEEzc',
-			},
-			{ website: 'fas fa-envelope', href: 'ieee@zewailcity.edu.eg' },
-		],
-		LogoBrand: './IEEE_Logo.png',
-		LogoBrand2: './IEEE_Logo_White.png',
-	};
-	return (
-		<Router>
-			<Navbar NavbarProps={NavbarProps} />
-			<Switch>
-				<Route exact path='/' component={LandingPage} />
-				<Route exact path='/about' component={AboutPage} />
-				<Route exact path='/magazine' component={MagazineListPage} />
-				<Route exact path='/blog' component={BlogListPage} />
-				<Route path='/blog/:id' component={BlogPage} />
-				<Route exact path='/juniors' component={Juniors} />
-				<Route exact path='/WIE' component={WIE} />
-			</Switch>
-		</Router>
-	);
+    ],
+    PageLink: "/",
+    isSearchable: false,
+    socialLinks: true,
+    Static: true,
+
+    socialAccounts: [
+      { website: "fab fa-facebook", href: "https://www.facebook.com/IEEEZC/" },
+      { website: "fab fa-twitter", href: "https://twitter.com/IEEEZC" },
+      { website: "fab fa-youtube", href: "https://www.youtube.com/user/IEEEzc" },
+      { website: "fas fa-envelope", href: "ieee@zewailcity.edu.eg" },
+    ],
+    LogoBrand: 'https://res.cloudinary.com/strapiieee/image/upload/v1599303256/IEEE_Logo_o54agg.png',
+    LogoBrand2: "https://res.cloudinary.com/strapiieee/image/upload/v1599303256/IEEE_Logo_White_rzntwy.png"
+
+
+  }
+  return (
+    <Router>
+      <Navbar NavbarProps={NavbarProps} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path='/magazine' component={MagazineListPage} />
+        <Route exact path="/blog" component={BlogListPage} />
+        <Route path="/blog/:id" component={BlogPage} />
+        <Route exact path="/juniors" component={Juniors} />
+        <Route exact path="/WIE" component={WIE} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- front-end/src/Components/Layout/Navbar.js
+++ front-end/src/Components/Layout/Navbar.js
@@ -2,110 +2,108 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
-
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { animateScroll as SScroll } from 'react-scroll';
-
 const Navbar = ({
-	NavbarProps: {
-		Static,
-		LogoBrand2,
-		NavItems,
-		PageLink,
-		isSearchable,
-		socialAccounts,
-		socialLinks,
-	},
+    NavbarProps: {
+        Static,
+        LogoBrand,
+        LogoBrand2,
+        NavItems,
+        PageLink,
+        isSearchable,
+        socialAccounts,
+        socialLinks,
+    },
 }) => {
-	return (
-		<>
-			<nav
-				className={`navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar`}
-				id='#portfolioNav'
-			>
-				<div className='container smooth-scroll'>
-					<Link
-						onClick={() => {
-							SScroll.scrollToTop();
-						}}
-						to={PageLink}
-					>
-						{' '}
-						<img
-							src={LogoBrand2}
-							style={{ height: '60px', objectFit: 'contain' }}
-							className='mine-logo'
-							alt=''
-							id='logoNav'
-						/>
-					</Link>
-					<button
-						className='navbar-toggler'
-						type='button'
-						data-toggle='collapse'
-						data-target='#navbarSupportedContent-7'
-						aria-controls='navbarSupportedContent-7'
-						aria-expanded='false'
-						aria-label='Toggle navigation'
-					>
-						<i className='fas fa-bars'></i>
-					</button>
-					<div
-						className='collapse navbar-collapse'
-						id='navbarSupportedContent-7'
-					>
-						<ul className='ml-5  navbar-nav mr-auto smooth-scroll'>
-							{Static
-								? NavItems.map((element) => {
-										return (
-											<li key={element.id} className='nav-item mx-3'>
-												<NavLink
-													className='nav-link'
-													exact
-													activeClassName='active font-weight-bold'
-													to={element.href}
-												>
-													{element.name}
-												</NavLink>
-											</li>
-										);
-								  })
-								: null}
-						</ul>
-						{isSearchable === true ? (
-							<form className='form-inline'>
-								<div className='md-form my-0'>
-									<input
-										className='form-control mr-sm-2'
-										type='text'
-										placeholder='Search'
-										aria-label='Search'
-									/>
-								</div>
-							</form>
-						) : null}
-						{socialLinks === true ? (
-							<ul className='navbar-nav nav-flex-icons'>
-								{socialAccounts.map((el) => {
-									return (
-										<li key={el.id} className='nav-item'>
-											<a
-												target='_blank'
-												href={el.href}
-												className='nav-link'
-											>
-												<i className={`fa-2x ${el.website}`}></i>
-											</a>
-										</li>
-									);
-								})}
-							</ul>
-						) : null}
-					</div>
-				</div>
-			</nav>
-		</>
-	);
+    return (
+        <>
+            <nav
+                className={`navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar`}
+                id='#portfolioNav'
+            >
+                <div className='container smooth-scroll'>
+                    <Link
+                        onClick={() => {
+                            SScroll.scrollToTop();
+                        }}
+                        to={PageLink}
+                    >
+                        {' '}
+                        <img
+                            src={LogoBrand2}
+                            style={{ width: '90px', height: '60px' }}
+                            className='mine-logo'
+                            alt=''
+                            id="logoNav"
+                            objectfit='contain'
+                        />
+                    </Link>
+                    <button
+                        className='navbar-toggler'
+                        type='button'
+                        data-toggle='collapse'
+                        data-target='#navbarSupportedContent-7'
+                        aria-controls='navbarSupportedContent-7'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <i className='fas fa-bars'></i>
+                    </button>
+                    <div
+                        className='collapse navbar-collapse'
+                        id='navbarSupportedContent-7'
+                    >
+                        <ul className='ml-5  navbar-nav mr-auto smooth-scroll'>
+                            {Static
+                                ? NavItems.map((element) => {
+                                    return (
+                                        <li key={element.id} className='nav-item mx-3'>
+                                            <Link
+                                                className='nav-link font-weight-bold'
+                                                to={element.href}
+                                            >
+                                                {element.name}
+                                            </Link>
+                                        </li>
+                                    );
+                                })
+                                : null}
+                        </ul>
+                        {isSearchable === true ? (
+                            <form className='form-inline'>
+                                <div className='md-form my-0'>
+                                    <input
+                                        className='form-control mr-sm-2'
+                                        type='text'
+                                        placeholder='Search'
+                                        aria-label='Search'
+                                    />
+                                </div>
+                            </form>
+                        ) : null}
+                        {socialLinks === true ? (
+                            <ul className='navbar-nav nav-flex-icons'>
+                                {socialAccounts.map((el) => {
+                                    return (
+                                        <li key={el.id} className='nav-item'>
+                                            <a
+                                                target='_blank'
+                                                href={el.href}
+                                                className='nav-link'
+                                            >
+                                                <i className={`fa-2x ${el.website}`}></i>
+                                            </a>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        ) : null}
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Navbar;
