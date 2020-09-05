188d7882d9fe5a6dc02ea479386d5f62bdc040ae
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-05 20:29:12 +0200

    add Video to WIE

--- front-end/src/Components/Pages/WIE/WIEIntro.js
+++ front-end/src/Components/Pages/WIE/WIEIntro.js
@@ -1,9 +1,17 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React,{useState} from 'react'
 import { Link } from 'react-scroll'
+import Modal from '../About/Modal'
 
 const WIEIntro = ({ WIEIntroProps }) => {
+    const [activeModal, setActiveModal] = useState(false);
     return (
         <>
+        <Modal
+				setActiveModal={setActiveModal}
+				activeModal={activeModal}
+			/>
             <div className="view" style={{ height: '100vh', backgroundImage: WIEIntroProps.img }}>
                 <div className="mask rgba-indigo-light">
                     <div className="container h-100 d-flex justify-content-center align-items-center">
@@ -22,8 +30,34 @@ const WIEIntro = ({ WIEIntroProps }) => {
                                                 {WIEIntroProps.text}
                                             </h5>
                                         </li>
+                                        <div
+											id='ytplayer2'
+											style={{ display: 'none' }}
+											className='mt-5'
+										>
+											<iframe
+												width='80%'
+												height='300px'
+												src='https://www.youtube.com/embed/SYlF2_9Zf6w'
+												allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+												allowFullScreen
+											></iframe>
+										</div>
                                         <li className="wow fadeIn" data-wow-delay="0.4s">
                                             <Link to="team-members-Wie" smooth={true} duration={1000} className="btn deep-orange pink-gradient btn-rounded btn-lg font-weight-bold">Get Started</Link>
+                                        
+                                            <button
+												data-toggle='modal'
+												data-target='#centralModal'
+												className='btn btn-white btn-rounded btn-lg   font-weight-bold ml-lg-0'
+												onClick={() => setActiveModal(true)}
+											>
+												<i
+													className='fas fa-play-circle orange-text mr-2'
+													aria-hidden='true'
+												></i>{' '}
+												Video
+											</button>
                                         </li>
                                     </ul>
                                 </div>
