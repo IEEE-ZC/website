4083109864eab710a8ba4e03d7c037058b2937ff
deviengeorge <deviengeorge99@gmail.com>
2020-09-09 13:17:14 +0200

    fixing credit cards and add header image in about page

--- front-end/src/Components/Pages/About/Credits.js
+++ front-end/src/Components/Pages/About/Credits.js
@@ -1,39 +1,58 @@
-/* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable jsx-a11y/img-redundant-alt */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React from 'react';
 
 const Credits = ({ CreditsData }) => {
-    return (
-        <>
-            <div className={`col-lg-6 col-md-12 mb-5 wow ${CreditsData && CreditsData.animation}`} data-wow-delay="0.2s">
+	return (
+		<>
+			<div
+				className={`col-lg-6 col-md-12 mb-3 wow ${CreditsData.animation}`}
+				data-wow-delay='0.2s'
+			>
+				<div className='row h-100'>
+					<div className='col-md-6'>
+						<img
+							src={CreditsData.image}
+							className='rounded z-depth-1 w-100 h-100'
+							style={{ objectFit: 'cover' }}
+							alt='First sample avatar'
+						/>
+					</div>
 
-                <div className="col-md-6 float-left">
-                    <div className="avatar mx-auto white mb-md-0 mb-4">
-                        <img src={CreditsData && CreditsData.image} className="rounded z-depth-1" alt="First sample avatar image" />
-                    </div>
-                </div>
+					<div
+						className='col-md-6 mt-3'
+					>
+						<h3 className='text-center'>
+							<strong>{CreditsData.name}</strong>
+						</h3>
+						<h6 className='text-center font-weight-bold grey-text mb-4'>
+							{CreditsData.job}
+						</h6>
+						<p className='text-center grey-text'>
+							{CreditsData.about}
+						</p>
 
-                <div className="col-md-6 float-right">
-                    <h3 className="text-center">
-                        <strong>{CreditsData && CreditsData.name}</strong>
-                    </h3>
-                    <h6 className="text-center font-weight-bold grey-text mb-4">{CreditsData && CreditsData.job}</h6>
-                    <p className="text-center grey-text">{CreditsData && CreditsData.about}</p>
+						<div className='d-flex flex-lg-wrap justify-content-center mt-2 text-center'>
+							{CreditsData.socialLinks.map((el) => {
+								return (
+									<a
+										key={el.website}
+										href={el.website}
+										className='p-2 m-2 fa-lg fb-ic'
+									>
+										<i
+											style={{ color: '#1d2d50' }}
+											className={`${el.icon} fa-2x`}
+										>
+											{' '}
+										</i>
+									</a>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-                    <div className='mt-5 text-center'>
-                        {CreditsData && CreditsData.socialLinks.map(el => {
-
-                            return (<a key={el.website} href={el.website} target="_blank" className="p-2 m-2 fa-lg fb-ic">
-                                <i style={{ color: '#1d2d50' }} className={`${el.icon} fa-2x`}> </i>
-                            </a>)
-                        })}
-                    </div>
-                </div>
-
-            </div>
-        </>
-    )
-}
-
-export default Credits
+export default Credits;
