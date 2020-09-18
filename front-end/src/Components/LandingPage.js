27be27e6482894d478d8109d6d89dd3c061c846c
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-18 08:54:58 +0200

    return to previous state

--- front-end/src/Components/LandingPage.js
+++ front-end/src/Components/LandingPage.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Intro from './Sections/Intro'
-
 import Events from './Sections/Events'
 import Achievements from './Sections/Achievements'
 import About from './Sections/About'
@@ -9,62 +8,52 @@ import CoreValue from './Sections/CoreValue'
 import WatchUs from './Sections/WatchUs'
 import ScrollSpy from './Sections/ScrollSpy'
 
-import { AnimatePresence } from 'framer-motion';
-import Preloader from '../Components/Layout/Preloader';
+
 
 import WOW from 'wowjs'
 const LandingPage = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		document.title = "IEEE| Home";
+		new WOW.WOW().init();
+	}, [])
 
 
-    const [loading, setLoading] = useState(true);
+	const IntroProps = {
+		Slogan: "Building Community"
+	}
+	const AboutProps = {
+		title: "A Glimpse",
+		text: " When a leader, be a serving bottle that flows into others' cups. You have the responsibility of filling your bottle with sufficient knowledge to flow it into their cups, and when you do; knowledge will prevail in the whole team. "
+		, img: "./imgs/glimpse/glimpse.jpg"
+	}
+	const ScrollSpyItems = {
+		NavItems: [
+			{ id: 1, name: "Home", href: "home" },
+			{ id: 2, name: "Events", href: "events" },
+			{ id: 3, name: "Achievements", href: "achievements" },
+			{ id: 4, name: "Glimpse", href: "about" },
+			{ id: 5, name: "Core Values", href: "core-values" },
+			{ id: 6, name: "Watch Us", href: "watch-us" },
+			{ id: 8, name: "Contact US", href: "contact-us" },
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        document.title = "IEEE| Home";
-        new WOW.WOW().init();
-    }, [])
+		]
+	}
 
-    useEffect(() => {
-        window.addEventListener('load', function () {
-            setLoading(false);
-        })
-    }, [])
-
-    const IntroProps = {
-        Slogan: "Building Community"
-    }
-    const AboutProps = {
-        title: "A Glimpse",
-        text: " When a leader, be a serving bottle that flows into others' cups. You have the responsibility of filling your bottle with sufficient knowledge to flow it into their cups, and when you do; knowledge will prevail in the whole team. "
-        , img: "./imgs/glimpse/glimpse.jpg"
-    }
-    const ScrollSpyItems = {
-        NavItems: [
-            { id: 1, name: "Home", href: "home" },
-            { id: 2, name: "Events", href: "events" },
-            { id: 3, name: "Achievements", href: "achievements" },
-            { id: 4, name: "Glimpse", href: "about" },
-            { id: 5, name: "Core Values", href: "core-values" },
-            { id: 6, name: "Watch Us", href: "watch-us" },
-            { id: 8, name: "Contact US", href: "contact-us" },
-
-        ]
-    }
-   
-    return (
-        <>
-            <AnimatePresence>{loading && <Preloader />}</AnimatePresence>
-            <Intro IntroProps={IntroProps} />
-            <Events />
-            {!loading && <ScrollSpy Items={ScrollSpyItems.NavItems} />}
-            <Achievements />
-            <About AboutProps={AboutProps} />
-            <CoreValue />
-            <WatchUs />
+	return (
+		<>
+			
+			<Intro IntroProps={IntroProps} />
+			<Events />
+			<ScrollSpy Items={ScrollSpyItems.NavItems} />
+			<Achievements />
+			<About AboutProps={AboutProps} />
+			<CoreValue />
+			<WatchUs />
 
 
-        </>
-    )
+		</>
+	)
 }
 
 export default LandingPage
